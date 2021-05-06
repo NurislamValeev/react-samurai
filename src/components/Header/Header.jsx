6164ae217428bec912cba0e0f83548020bff4f4f
Nurislam <nurislamvaleev@gmail.com>
2021-05-06 04:24:02 +0300

    added authReducer, made HeaderContainer as a class component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,22 @@
 import React from "react"
 import s from "./Header.module.css"
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
-	return (
-		<header className={s.header}>
-			<img
-				className={s.rotate}
-				src={require("../../img/reactjs-icon.svg")}
-				alt=''
-			/>
-		</header>
-	)
+const Header = (props) => {
+   return (
+      <header className={s.header}>
+         <img
+            className={s.rotate}
+            src={require("../../img/reactjs-icon.svg")}
+            alt=''
+         />
+         <div className={s.loginBlock}>
+            {props.isAuth ? props.login :
+               <NavLink to={'/login'}>Login</NavLink>
+            }
+         </div>
+      </header>
+   )
 }
 
 export default Header
