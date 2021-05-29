bdf87f5cc50c98e1902393141e14eb44f8fde1df
Nurislam <nurislamvaleev@gmail.com>
2021-05-29 20:34:40 +0300

    Updates

---
+++ precache-manifest.538c7f11d30c058585ba7699b1a4ec5e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c158891b38896221d9cb",
+    "url": "/react-samurai/static/css/main.4258cd4e.chunk.css"
+  },
+  {
+    "revision": "c158891b38896221d9cb",
+    "url": "/react-samurai/static/js/main.c158891b.chunk.js"
+  },
+  {
+    "revision": "a80cbba057d281588f3d",
+    "url": "/react-samurai/static/css/1.2fe4f158.chunk.css"
+  },
+  {
+    "revision": "a80cbba057d281588f3d",
+    "url": "/react-samurai/static/js/1.a80cbba0.chunk.js"
+  },
+  {
+    "revision": "331a59ab573886f0ed9a",
+    "url": "/react-samurai/static/css/2.40ec2b33.chunk.css"
+  },
+  {
+    "revision": "331a59ab573886f0ed9a",
+    "url": "/react-samurai/static/js/2.331a59ab.chunk.js"
+  },
+  {
+    "revision": "9c913afcfa51c8274a70",
+    "url": "/react-samurai/static/js/3.9c913afc.chunk.js"
+  },
+  {
+    "revision": "f21312fcd93e61d95613",
+    "url": "/react-samurai/static/js/4.f21312fc.chunk.js"
+  },
+  {
+    "revision": "af1c0831c14fd3bfc275",
+    "url": "/react-samurai/static/js/runtime~main.af1c0831.js"
+  },
+  {
+    "revision": "7ea4af7d8401d2b43ee841bfa2abe89d",
+    "url": "/react-samurai/static/media/user-icon.7ea4af7d.png"
+  },
+  {
+    "revision": "f098b7c8ad29d88ca59c60c389ecf328",
+    "url": "/react-samurai/static/media/spinner.f098b7c8.svg"
+  },
+  {
+    "revision": "7038477259b6f1696308f867ce8c0a3a",
+    "url": "/react-samurai/static/media/reactjs-icon.70384772.svg"
+  },
+  {
+    "revision": "621b794b725c8ac0d4eb67c9e48dd00e",
+    "url": "/react-samurai/index.html"
+  }
+];
