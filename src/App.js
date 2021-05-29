196eb721f0e78b680679d5f32b173afe4f36dd3b
Nurislam <nurislamvaleev@gmail.com>
2021-05-29 21:07:18 +0300

    added gh-pages, BrowserRouter basename

--- src/App.js
+++ src/App.js
@@ -60,9 +60,8 @@ const AppContainer = compose(
    connect(mapStateToProps, {initializeApp}))(App)
 
 const SamuraiJSApp = (props) => {
-
    return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
          <Provider store={store}>
             <AppContainer/>
          </Provider>
